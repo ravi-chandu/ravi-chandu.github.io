e94f7eed063d03fdbff54987b01b1ed614713611
Ravi chandu <92777166+ravi-chandu@users.noreply.github.com>
2021-11-05 22:28:11 +0530

    Create gtag.js

---
+++ gtag.js
@@ -0,0 +1,9 @@
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-S742QH4H62"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-S742QH4H62');
+</script>
