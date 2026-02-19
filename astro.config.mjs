73c7966404136ea3bd34a360ffbf41221c8e49eb
ravi-chandu <ravichandu464@gmail.com>
2026-02-19 23:27:28 +0530

    Remove astro-icon dependency (using emoji instead)

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,6 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import react from '@astrojs/react';
 
-import icon from 'astro-icon';
-
 // https://astro.build/config
 export default defineConfig({
   site: 'https://ravi-chandu.github.io',
@@ -12,5 +10,5 @@ export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
   },
-  integrations: [react(), icon()]
+  integrations: [react()]
 });
